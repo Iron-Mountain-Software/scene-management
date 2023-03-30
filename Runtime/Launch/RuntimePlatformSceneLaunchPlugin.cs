d8545c4b284e3519be88e25447a493fdc5d3d5a1
chris-spellbound <chris@spellboundar.com>
2023-03-30 09:41:34 -0400

    runtime platofrm launching

---
+++ Runtime/Launch/RuntimePlatformSceneLaunchPlugin.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+namespace SpellBoundAR.SceneManagement.Launch
+{
+    public class RuntimePlatformSceneLaunchPlugin : MonoBehaviour, ISceneLaunchPlugin
+    {
+        public event Action OnStatusMessageChanged;
+        
+        [SerializeField] private int priority;
+        [SerializeField] private SceneData sceneToLaunchAndroid;
+        [SerializeField] private SceneData sceneToLaunchIPhonePlayer;
+        [SerializeField] private SceneData sceneToLaunchWebGLPlayer;
+        [SerializeField] private SceneData sceneToLaunchDefault;
+        [SerializeField] private string statusMessage;
+
+        public int Priority => priority;
+        public bool IsReady => true;
+        public string StatusMessage => statusMessage;
+        public SceneData SceneToLaunch
+        {
+            get
+            {
+                switch (Application.platform)
+                {
+                    case RuntimePlatform.Android:
+                        return sceneToLaunchAndroid;
+                    case RuntimePlatform.IPhonePlayer:
+                        return sceneToLaunchIPhonePlayer;
+                    case RuntimePlatform.WebGLPlayer:
+                        return sceneToLaunchWebGLPlayer;
+                    default:
+                        return sceneToLaunchDefault;
+                }
+            }
+        }
+    }
+}
